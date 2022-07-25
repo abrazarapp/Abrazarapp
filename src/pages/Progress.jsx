956899e43bd209f07bf640884902c16797663455
joan <joanmutti1090@gmail.com>
2022-07-25 17:45:37 -0300

    remove unused vars

--- src/pages/Progress.jsx
+++ src/pages/Progress.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import NavigationLayout from "../containers/NavigationLayout";
 import ProgressBars from "../components/ProgressBars";
 import useProgress from "../hooks/useProgress";
