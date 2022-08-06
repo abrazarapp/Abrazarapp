894daed7bf4e3586848d1add668c0152b29147ac
joan <joanmutti1090@gmail.com>
2022-08-06 08:53:55 -0300

    Analytics

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -21,7 +21,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
 
 export const auth = getAuth();
 export const db = getFirestore(app);
